430edaf93e260f32124758beacb46fc6c17df7fc
Xun Li <lixun910@gmail.com>
2025-10-13 21:52:23 -0700

    clean up
    Signed-off-by: Xun Li <lixun910@gmail.com>

--- examples/vercel_leaflet_example/next-env.d.ts
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-// Copyright contributors to the openassistant project
-
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
--- examples/vercel_v5_client_only/postcss.config.js
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-// Copyright contributors to the openassistant project
-
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
